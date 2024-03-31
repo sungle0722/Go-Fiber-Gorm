ee3b08ff4fcd0e32a86e85329971eab47b3fde9d
SungLee <82447197+sungle0722@users.noreply.github.com>
2024-03-31 22:57:24 +0200

    Update main.go

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func main() {
 	models.Setup(db)
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hej - Watashi!")
+		return c.SendString("Hej - Watashi - Why why?!")
 	})
 
 	log.Fatal(app.Listen(getPort()))
